1a42ad50a3212ad47e129924b9a75a0d7573fc49
svrxo <info.sohaib.fares@gmail.com>
2025-11-25 14:25:33 +0100

    feat: use tcp-listener to test requests

---
+++ cmd/tcplistener/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/red7-c/httpfromtcp/internal/request"
+)
+
+func main() {
+	l, err := net.Listen("tcp", ":42069")
+	if err != nil {
+		log.Fatal("error", err)
+	}
+	defer l.Close()
+	for {
+		con, err := l.Accept()
+		if err != nil {
+			log.Fatal("error", err)
+		}
+		req, err := request.RequestFromReader(con)
+		if err != nil {
+			log.Fatal("error", err)
+		}
+		fmt.Printf("Request line:\n")
+		fmt.Printf("- Method: %s\n", req.RequestLine.Method)
+		fmt.Printf("- Target: %s\n", req.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %s\n",req.RequestLine.HttpVersion)
+	}
+}
