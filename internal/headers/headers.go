d5a1afba1d9f09c2fa95a3ce023b1a46bb538f0e
svrxo <info.sohaib.fares@gmail.com>
2025-11-26 00:59:32 +0100

    feat: add http header parsing and validation

---
+++ internal/headers/headers.go
@@ -0,0 +1,106 @@
+package headers
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
+func parseHeader(fieldLine []byte) (string, string, error) {
+	parts := bytes.SplitN(fieldLine, []byte(":"), 2)
+	if len(parts) != 2 {
+		return "", "", ErrorMalformedFieldLine
+	}
+	name := parts[0]
+	value := bytes.TrimSpace(parts[1])
+
+	if bytes.HasSuffix(name, []byte(" ")) {
+		return "", "", ErrorMalformedFieldName
+	}
+	return string(name), string(value), nil
+}
+
+func isToken(str []byte) bool {
+	for _, ch := range str {
+		found := false
+		if ch >= 'A' && ch <= 'Z' ||
+			ch >= 'a' && ch <= 'z' ||
+			ch >= '0' && ch <= '9' {
+			found = true
+		}
+		switch ch {
+		case '!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~':
+			found = true
+		}
+		if !found {
+			return false
+		}
+	}
+	return true
+}
+
+type Headers struct {
+	headers map[string]string
+}
+
+var ErrorMalformedFieldLine = fmt.Errorf("malformed field-line")
+var ErrorMalformedFieldName = fmt.Errorf("malformed field name")
+var CRLF = []byte("\r\n")
+
+func NewHeaders() *Headers {
+	return &Headers{
+		headers: map[string]string{},
+	}
+}
+
+func (h *Headers) Get(name string) string {
+	return h.headers[strings.ToLower(name)]
+}
+
+func (h *Headers) Set(name, value string) {
+	name = strings.ToLower(name)
+
+	if v, ok := h.headers[name]; ok {
+		h.headers[name] = fmt.Sprintf("%s,%s", v, value)
+	} else {
+		h.headers[name] = value
+	}
+
+}
+
+func (h *Headers) ForEach(cb func(n, v string)) {
+	for n, v := range h.headers {
+		cb(n, v)
+	}
+}
+
+func (h *Headers) Parse(data []byte) (int, bool, error) {
+	read := 0
+	done := false
+
+	for {
+		idx := bytes.Index(data[read:], CRLF)
+		if idx == -1 {
+			break
+		}
+		//EMPTY header
+		if idx == 0 {
+			done = true
+			read += len(CRLF)
+			break
+		}
+		name, value, err := parseHeader(data[read : read+idx])
+		if err != nil {
+			return 0, false, err
+		}
+
+		if !isToken([]byte(name)) {
+			return 0, false, ErrorMalformedFieldName
+		}
+
+		read += idx + len(CRLF)
+		h.Set(name, value)
+
+	}
+	return read, done, nil
+}
---
+++ internal/headers/headers_test.go
@@ -0,0 +1,39 @@
+package headers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHeaderParse(t *testing.T) {
+	// Test: Valid single header
+	headers := NewHeaders()
+	data := []byte("Host: localhost:42069\r\nFooFoo:      barbar   \r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers.Get("Host"))
+	assert.Equal(t, "barbar", headers.Get("FooFoo"))
+	assert.Equal(t, "", headers.Get("MissingKey"))
+	assert.Equal(t, 49, n)
+	assert.True(t, done)
+
+	// Test: Invalid token in header name
+	headers = NewHeaders()
+	data = []byte("H©st: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+	
+	// Test: Valid multiple values
+	headers = NewHeaders()
+	data = []byte("Host: localhost:42069\r\nHost: localhost:42069\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069,localhost:42069", headers.Get("HOST"))
+	assert.False(t, done)
+}
