cd1a0327999ba4c1eccf10b805565164ee58e51a
svrxo <info.sohaib.fares@gmail.com>
2025-11-26 01:03:08 +0100

    ref: test both req and field lines

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -26,6 +26,11 @@ func main() {
 		fmt.Printf("Request line:\n")
 		fmt.Printf("- Method: %s\n", req.RequestLine.Method)
 		fmt.Printf("- Target: %s\n", req.RequestLine.RequestTarget)
-		fmt.Printf("- Version: %s\n",req.RequestLine.HttpVersion)
+		fmt.Printf("- Version: %s\n", req.RequestLine.HttpVersion)
+		fmt.Printf("- Headers:\n")
+
+		req.Headers.ForEach(func(n, v string) {
+			fmt.Printf("- %s: %s\n", n, v)
+		})
 	}
 }
